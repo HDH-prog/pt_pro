3d91be4fd956e19a203b4b89acec37dfc4c9fe9f
Donghu <eastking34@gmail.com>
2025-08-01 11:29:02 +0900

    feat: 회원상세페이지 생성

---
+++ lib/ui/pages/member_detail/member_detail_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MemberDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Text('MemeberDetailPage'));
+  }
+}
