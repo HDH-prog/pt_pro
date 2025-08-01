798d4e4a4066bbbae2ee3c349d9631ae1a2d22ed
Donghu <eastking34@gmail.com>
2025-08-01 11:19:27 +0900

    Fix: 모델클래스 추가

--- lib/model/member.dart
+++ lib/model/member.dart
@@ -14,6 +14,7 @@ class Member {
   DateTime? sessionEndDate; // PT 수업 종료일
   String ptGoal; // PT 목표 (예: 다이어트, 체력 향상)
   int sessionCount; // 수업 횟수
+  int currentSessionCount; // 현재 수업 횟수
   String? notes; // 특이사항 (예: 무릎 통증 등)
   DateTime createdAt; // 데이터 생성일
   DateTime updatedAt; // 데이터 마지막 수정일
@@ -30,6 +31,7 @@ class Member {
     this.sessionEndDate,
     required this.ptGoal,
     required this.sessionCount,
+    required this.currentSessionCount,
     this.notes,
     required this.createdAt,
     required this.updatedAt,
